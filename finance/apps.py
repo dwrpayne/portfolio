e7f6d08ae04b2060f60afda9b62de31bc2d60817
David Payne <dwrpayne@gmail.com>
2017-11-28 01:30:07 -0800

    Plotly creds from env

--- finance/apps.py
+++ finance/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+import os
+import plotly
 
 class financeConfig(AppConfig):
     name = 'finance'
+    
+    def ready(self):
+        plotly.tools.set_credentials_file(username=os.environ.get('PLOTLY_USERNAME'), api_key=os.environ.get('PLOTLY_API_KEY'))
