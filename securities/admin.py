819926959dbf73f145efa5f17749493ef57cd726
David Payne <dwrpayne@gmail.com>
2017-12-17 13:47:49 -0800

    Remove old fixture.

--- securities/admin.py
+++ securities/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Security, SecurityPrice
 
+
 class SecurityAdmin(admin.ModelAdmin):
     def latest_update_day(self, obj):
         return obj.prices.latest().day
 
     def first_update_day(self, obj):
         return obj.prices.earliest().day
+
     latest_update_day.short_description = "Latest Price"
     list_filter = ['currency', 'type']
     list_display = ['symbol', 'type', 'currency', 'live_price',
@@ -22,4 +24,3 @@ class SecurityPriceAdmin(admin.ModelAdmin):
 
 
 admin.site.register(SecurityPrice, SecurityPriceAdmin)
-
