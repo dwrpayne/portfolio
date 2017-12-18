4fe3507450ee54555a9d9780beac66cd540e77c4
David Payne <dwrpayne@gmail.com>
2017-12-18 01:45:09 -0800

    Abstract daterange

--- utils/dates.py
+++ utils/dates.py
@@ -1,20 +1,25 @@
 import datetime
-import pendulum
+
+import arrow
+from pendulum import Date
 
 
 def week_ends(start, end=datetime.date.today()):
-    period = (pendulum.Date.instance(start) - pendulum.Date.instance(end))
+    period = (Date.instance(start) - Date.instance(end))
     for week in period.range('weeks'):
-        yield min(week.end_of('week'), pendulum.Date.today())
+        yield min(week.end_of('week'), Date.today())
 
 
 def month_ends(start, end=datetime.date.today()):
-    period = (pendulum.Date.instance(start) - pendulum.Date.instance(end))
+    period = (Date.instance(start) - Date.instance(end))
     for month in period.range('months'):
-        yield min(month.end_of('month'), pendulum.Date.today())
+        yield min(month.end_of('month'), Date.today())
 
 
 def year_ends(start, end=datetime.date.today()):
-    period = (pendulum.Date.instance(start) - pendulum.Date.instance(end))
+    period = (Date.instance(start) - Date.instance(end))
     for year in period.range('years'):
-        yield min(year.end_of('year'), pendulum.Date.today())
+        yield min(year.end_of('year'), Date.today())
+
+def day_intervals(days, start, end=datetime.date.today()):
+    return arrow.Arrow.interval('day', arrow.get(start), arrow.get(end), days)
