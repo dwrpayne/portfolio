4e1431f051b5f5f4e81f317e2e0f092a92e166c5
David Payne <dwrpayne@gmail.com>
2017-12-22 17:00:46 -0800

    Get yahoo data for a stock

--- datasource/services.py
+++ datasource/services.py
@@ -1,6 +1,7 @@
-import requests
 from decimal import Decimal
 import json
+import requests
+
 
 # TODO: Yuck, this is really hacky and needs a refactor. I just need it working for now.
 # TODO: Maybe a ManyToMany field to support multiple data sources per security?
@@ -16,12 +17,21 @@ def GetLiveAlphaVantageExchangeRate(symbol):
         print('Failed to get data, response: {}'.format(r.content))
     return None
 
-def GetYahooStockInformation(symbol):
+def GetYahooStockData(symbol):
+    print('Refreshing metadata for ' + symbol)
     req = requests.get('https://finance.yahoo.com/quote/' + symbol)
     text = req.text
-i1=0
-i1=r.find('root.App.main', i1)
-i1=r.find('{', i1)
-i2=r.find("\n", i1)
-i2=r.rfind(';', i1, i2)
-jsonstr=r[i1:i2]
+    i1 = 0
+    i1 = text.find('root.App.main', i1)
+    i1 = text.find('{', i1)
+    i2 = text.find("\n", i1)
+    i2 = text.rfind(';', i1, i2)
+    jsonstr = text[i1:i2]
+    j = json.loads(jsonstr)
+    for tag in ['context', 'dispatcher', 'stores', 'QuoteSummaryStore']:
+        if not tag in j:
+            print('Missing json tag {}, skipping...'.format(tag))
+            return {}
+        j = j[tag]
+    return j
+
