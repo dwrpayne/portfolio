23b49a7c7f3d7b68600948bd2528e5eea29a3f15
David Payne <dwrpayne@gmail.com>
2017-12-23 13:06:39 -0800

    Full switch over to Pendulum.

--- utils/dates.py
+++ utils/dates.py
@@ -1,25 +1,29 @@
 import datetime
-
-import arrow
 from pendulum import Date
 
 
-def week_ends(start, end=datetime.date.today()):
+def week_ends(start, end=None):
+    if not end: end = datetime.date.today()
     period = (Date.instance(start) - Date.instance(end))
     for week in period.range('weeks'):
         yield min(week.end_of('week'), Date.today())
 
 
-def month_ends(start, end=datetime.date.today()):
+def month_ends(start, end=None):
+    if not end: end = datetime.date.today()
     period = (Date.instance(start) - Date.instance(end))
     for month in period.range('months'):
         yield min(month.end_of('month'), Date.today())
 
 
-def year_ends(start, end=datetime.date.today()):
+def year_ends(start, end=None):
+    if not end: end = datetime.date.today()
     period = (Date.instance(start) - Date.instance(end))
     for year in period.range('years'):
         yield min(year.end_of('year'), Date.today())
 
-def day_intervals(days, start, end=datetime.date.today()):
-    return arrow.Arrow.interval('day', arrow.get(start), arrow.get(end), days)
+
+def day_intervals(days, start, end=None):
+    if not end: end = datetime.date.today()
+    period = Date.instance(end) - Date.instance(start)
+    return period.split('day', days)
