19575d4a1a53d3a1002c6f749f1b3faed1668f21
David Payne <dwrpayne@gmail.com>
2018-01-06 23:27:56 -0800

    Initial datasource tests

--- datasource/tests.py
+++ datasource/tests.py
@@ -1,1 +1,24 @@
 # Create your tests here.
+from django.test import TestCase
+from datasource.models import DataSourceMixin
+from datetime import date
+
+class DataSourceMixinTestCase(TestCase):
+
+    def test_process_basic(self):
+        pairs = [(date(2017, 1, 1), 12.34), (date(2017, 1, 2), 12.12),
+                 (date(2017, 1, 3), 12.98), (date(2017, 1, 4), 13.50)]
+        series = list(DataSourceMixin._ProcessRateData(pairs, date(2017, 1, 4)))
+        self.assertEqual(series, pairs)
+
+    def test_process_gaps(self):
+        pairs = [(date(2017, 1, 1), 12.34), (date(2017, 1, 3), 12.98), (date(2017, 1, 4), 13.50)]
+        series = list(DataSourceMixin._ProcessRateData(pairs, date(2017, 1, 4)))
+        self.assertEqual(series[0], pairs[0])
+        self.assertEqual(series[1], (date(2017, 1, 2), 12.34))
+        self.assertEqual(series[2], pairs[1])
+        self.assertEqual(series[3], pairs[2])
+
+        
+
+
