c4b569f5eff72163e796710cdec167cf4941c5ba
David Payne <dwrpayne@gmail.com>
2018-03-02 23:40:16 -0800

    Reenable caching.

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
