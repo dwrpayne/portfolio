aa55fc564e58f27024bf37dfc8033745b0fbfd43
David Payne <dwrpayne@gmail.com>
2018-03-06 18:36:47 -0800

    Ratelimit AV to every 4 seconds.

--- datasource/models.py
+++ datasource/models.py
@@ -101,7 +101,7 @@ class AlphaVantageStockSource(DataSourceMixin):
                 return
         self.symbol = 'NO VALID LOOKUP FOR {}'.format(original_symbol)
 
-    @rate_limited(1, 2)
+    @rate_limited(1, 4)
     def _Retrieve(self, start, end):
         params = {'function': 'TIME_SERIES_DAILY', 'apikey': self.api_key,
                   'symbol': self.symbol}
