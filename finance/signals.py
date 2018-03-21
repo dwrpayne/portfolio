b08f7b8a8928515972d8ac48a5bf1ba0f2b2d28b
David Payne <dwrpayne@gmail.com>
2018-03-20 20:37:32 -0700

    Create allocation when creating new holdings.

--- finance/signals.py
+++ finance/signals.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .models import Holding, Allocation
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_profile(sender, instance, **kwargs):
-    user = instance
-    if kwargs["created"]:
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
         from .models import UserProfile
-        user_profile = UserProfile(user=user)
-        user_profile.save()
+        UserProfile.objects.create(user=user)
+
+@receiver(post_save, sender=Holding)
+def create_holding(sender, instance, created, **kwargs):
+    if True:#created:
+        holding = instance
+        Allocation.objects.ensure_allocated(holding.security_id, holding.account.user)
